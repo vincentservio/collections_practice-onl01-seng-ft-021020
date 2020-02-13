3c5968f1d875632b1387890943dc2814fddf0522
Vincent Servio <vincentservio@gmail.com>
2020-02-13 21:20:25 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,18 @@
 
+def sort_array_asc(i)
+  i.sort
+  
+end 
+def sort_array_desc(d)
+  d.sort.reverse
+end 
+
+def sort_array_char_count(c)
+  c.sort do |a, b|
+  a.length <=> b.length
+end
+end 
+
+def swap_elements(n)
+  
+end 
